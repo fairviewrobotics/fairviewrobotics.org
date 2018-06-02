d4e984cb7649b7b3f8ed1dcd62fd4686f9873ad4
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-02 15:00:59 -0600

    Update Header BSNavBar usage

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar as BSNavBar, Nav, NavItem } from 'react-bootstrap';
 import * as bootstrapUtils from "react-bootstrap/lib/utils/bootstrapUtils";
 import { NavLink } from 'react-router-dom';
 // This helps link 'react-router-dom' and 'react-bootstrap' together to make links work
@@ -13,7 +13,7 @@ import './Header.css';
 import logo from '../images/logo.png';
 
 // This remove the default styling from 'react-bootstrap' for the Navbar
-bootstrapUtils.addStyle(Navbar, 'none');
+bootstrapUtils.addStyle(BSNavBar, 'none');
 
 class Header extends Component {
 
@@ -25,18 +25,18 @@ class Header extends Component {
     const collapsedStyling = this.props.isCollapsed ? 'top-nav-collapse' : null;
 
     return (
-      <Navbar collapseOnSelect fixedTop bsStyle="none" className={`nav-custom ${collapsedStyling}`}>
-        <Navbar.Header>
-          <Navbar.Brand>
+      <BSNavBar collapseOnSelect fixedTop bsStyle="none" className={`nav-custom ${collapsedStyling}`}>
+        <BSNavBar.Header>
+          <BSNavBar.Brand>
             <NavLink to="/">
               <img src={logo} alt="logo" className="header-logo" />
             </NavLink>
-          </Navbar.Brand>
-          <Navbar.Toggle>
+          </BSNavBar.Brand>
+          <BSNavBar.Toggle>
             <FontAwesomeIcon icon={faBars}/>
-          </Navbar.Toggle>
-        </Navbar.Header>
-        <Navbar.Collapse>
+          </BSNavBar.Toggle>
+        </BSNavBar.Header>
+        <BSNavBar.Collapse>
           <Nav pullRight id="main-nav">
             <LinkContainer to="/about">
               <NavItem>About</NavItem>
@@ -51,8 +51,8 @@ class Header extends Component {
               <NavItem>Calendar</NavItem>
             </LinkContainer>
           </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        </BSNavBar.Collapse>
+      </BSNavBar>
     );
   }
 }
