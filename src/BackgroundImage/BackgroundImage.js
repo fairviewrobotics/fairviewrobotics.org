ba8f5c9f174f7ab1eda166d5b6e121fd887b2615
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-07 11:40:40 +0200

    Fix BackgroundImage positioning
    This fix removes need for pages to have a margin-top to render correctly.

--- src/BackgroundImage/BackgroundImage.js
+++ src/BackgroundImage/BackgroundImage.js
@@ -1,47 +1,52 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './BackgroundImage.module.css';
 
-export default class BackgroundImage extends PureComponent {
-
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    fullScreen: PropTypes.bool,
-    animate: PropTypes.shape({
-      duration: PropTypes.number.isRequired,
-      delay: PropTypes.number.isRequired
-    })
+const BackgroundImage = ({ src, fullScreen, animate }) => {
+  const imageStyle = {
+    backgroundImage: `url(${src})`
   };
 
-  static defaultProps = {
-    fullScreen: false
-  };
-
-  render() {
-    const imageStyle = {
-      backgroundImage: `url(${this.props.src})`
-    };
-
-    if (!this.props.fullScreen) {
-      imageStyle.position = 'absolute';
-      imageStyle.backgroundPosition = '50% 75px';
+  const animationStyle = animate ?
+    {
+      transition: `background-image ${animate.duration}ms, background-size 0s`,
+      transitionDelay: `${animate.delay}ms`
     }
-    const fullScreenClass = !this.props.fullScreen ? styles.notFullscreen : '';
-    
-    const animationStyle = this.props.animate ?
-      {
-        transition: `background-image ${this.props.animate.duration}ms, background-size 0s`,
-        transitionDelay: `${this.props.animate.delay}ms`
-      }
-      :
-      null;
+    :
+    null;
 
-    return (
-      <div>
-        <div className={`${styles.introMask} ${fullScreenClass}`} />
-        <div className={`${styles.imageParallax} ${fullScreenClass}`} style={{ ...imageStyle, ...animationStyle }} />
+  const parallaxStyle = {...animationStyle, ...imageStyle};
+
+  let backgroundImage = (
+    <Fragment>
+      <div className={styles.introMask} />
+      <div className={styles.imageParallax} style={parallaxStyle} />
+    </Fragment>
+  );
+
+  if (!fullScreen) {
+    backgroundImage = (
+      <div className={styles.imageContainer}>
+        {backgroundImage}
       </div>
     );
   }
-}
+
+  return backgroundImage;
+};
+
+BackgroundImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  fullScreen: PropTypes.bool,
+  animate: PropTypes.shape({
+    duration: PropTypes.number.isRequired,
+    delay: PropTypes.number.isRequired
+  })
+};
+
+BackgroundImage.defaultProps = {
+  fullScreen: false
+};
+
+export default BackgroundImage;
